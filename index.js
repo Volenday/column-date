f2af2b479b3dd3cd5ed796f698e1cc0cee41eee5
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-05-30 09:45:42 +0800

    v0.0.1

---
+++ index.js
@@ -0,0 +1,217 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import InputDate from '@volenday/input-date';
+import { Calendar, DateRangePicker } from 'react-date-range';
+import { Pane, Popover } from 'evergreen-ui';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+
+export default props => {
+	const {
+		editable = false,
+		fieldType,
+		filterType = 'date',
+		headerStyle = {},
+		id,
+		onChange,
+		onChangeText,
+		style = {},
+		...defaultProps
+	} = props;
+	let { defaultValue } = props;
+
+	if (defaultValue == '') {
+		if (filterType == 'date') {
+			defaultValue = null;
+		}
+	}
+
+	let momentFormat = 'MMMM DD, YYYY';
+	switch (fieldType) {
+		case 'datetime':
+			momentFormat = 'MMMM DD, YYYY hh:mm A';
+			break;
+		case 'time':
+			momentFormat = 'hh:mm A';
+			break;
+	}
+
+	if (typeof defaultValue == 'string') {
+		if (filterType == 'date') {
+			if (defaultValue) {
+				defaultValue = moment(defaultValue);
+			}
+		}
+	}
+
+	return {
+		...defaultProps,
+		style: { ...style, display: 'flex', alignItems: 'center' },
+		headerStyle: { ...headerStyle, display: 'flex', alignItems: 'center' },
+		Cell: ({ index, original, value }) => {
+			if (editable) {
+				return (
+					<div class="input-group" style={{ width: '100%' }}>
+						<InputDate
+							id={id}
+							value={value}
+							onChange={(field, value) => onChangeText(index, field, value)}
+							withTime={true}
+						/>
+						<div
+							class="input-group-addon"
+							onClick={e => onChange({ Id: original.Id, [id]: value })}
+							style={{ cursor: 'pointer' }}>
+							<i class="fa fa-floppy-o" aria-hidden="true" />
+						</div>
+					</div>
+				);
+			}
+
+			return <span>{moment(value).isValid() ? moment(value).format(momentFormat) : null}</span>;
+		},
+		Filter: ({ onChange }) => {
+			if (filterType == 'date') {
+				return (
+					<Popover
+						content={() => (
+							<Pane width={332} height={343}>
+								<Calendar date={defaultValue} onChange={e => onChange(moment(e).toJSON())} />
+							</Pane>
+						)}>
+						{({ getRef, toggle }) => {
+							return (
+								<div class="input-group" ref={getRef} style={{ width: '100%' }}>
+									{defaultValue && (
+										<div
+											class="input-group-addon"
+											onClick={e => onChange('')}
+											style={{ cursor: 'pointer' }}>
+											<i class="fa fa-times" aria-hidden="true" />
+										</div>
+									)}
+									<input
+										type="text"
+										class="form-control"
+										placeholder="Select Date"
+										readOnly
+										onClick={e => toggle()}
+										value={defaultValue ? defaultValue.format('YYYY/MM/DD') : ''}
+									/>
+									<div
+										class="input-group-addon"
+										onClick={e => toggle()}
+										style={{ cursor: 'pointer' }}>
+										<i class="fa fa-calendar" aria-hidden="true" />
+									</div>
+								</div>
+							);
+						}}
+					</Popover>
+				);
+			}
+
+			if (filterType == 'dateRange') {
+				return <CustomDateRange defaultValue={defaultValue} filterType={filterType} onChange={onChange} />;
+			}
+
+			return null;
+		}
+	};
+};
+
+class CustomDateRange extends Component {
+	state = { endDate: false, focusedRange: [0, 0], startDate: false };
+
+	render() {
+		const { endDate, focusedRange, startDate } = this.state;
+		let { defaultValue, onChange } = this.props;
+
+		if (defaultValue == '') {
+			defaultValue = {
+				startDate: startDate ? moment(startDate) : false,
+				endDate: endDate ? moment(endDate) : false,
+				key: 'selection'
+			};
+		} else {
+			if (startDate || endDate) {
+				defaultValue = {
+					startDate: startDate ? moment(startDate) : false,
+					endDate: endDate ? moment(endDate) : false,
+					key: 'selection'
+				};
+			}
+		}
+
+		if (typeof defaultValue == 'string') {
+			if (defaultValue.includes('*')) {
+				const defaultValueSplit = defaultValue.split('*'),
+					defaultValueStartDate = defaultValueSplit[0],
+					defaultValueEndDate = defaultValueSplit[1];
+				defaultValue = {
+					startDate: moment(defaultValueStartDate),
+					endDate: moment(defaultValueEndDate),
+					key: 'selection'
+				};
+			}
+		}
+
+		return (
+			<Popover
+				content={() => (
+					<Pane width={560} height={345}>
+						<DateRangePicker
+							ranges={[defaultValue]}
+							onChange={async e => {
+								const startDate = moment(e.selection.startDate).toJSON(),
+									endDate = moment(e.selection.endDate).toJSON();
+
+								await this.setState({ endDate, startDate });
+
+								if (
+									(startDate != '' && endDate != '' && focusedRange[1] == 1) ||
+									(startDate != endDate && focusedRange[1] == 0)
+								) {
+									onChange(`${startDate}*${endDate}`);
+								}
+							}}
+							focusedRange={focusedRange}
+							onRangeFocusChange={e => this.setState({ focusedRange: e })}
+						/>
+					</Pane>
+				)}>
+				{({ getRef, toggle }) => {
+					return (
+						<div class="input-group" ref={getRef}>
+							{defaultValue.startDate && (
+								<div
+									class="input-group-addon"
+									onClick={e => onChange('')}
+									style={{ cursor: 'pointer' }}>
+									<i class="fa fa-times" aria-hidden="true" />
+								</div>
+							)}
+							<input
+								type="text"
+								class="form-control"
+								placeholder="Select Date Range"
+								readOnly
+								onClick={e => toggle()}
+								value={
+									defaultValue.startDate
+										? `${defaultValue.startDate.format(
+												'YYYY/MM/DD'
+										  )} - ${defaultValue.endDate.format('YYYY/MM/DD')}`
+										: ''
+								}
+							/>
+							<div class="input-group-addon" onClick={e => toggle()} style={{ cursor: 'pointer' }}>
+								<i class="fa fa-calendar" aria-hidden="true" />
+							</div>
+						</div>
+					);
+				}}
+			</Popover>
+		);
+	}
+}
